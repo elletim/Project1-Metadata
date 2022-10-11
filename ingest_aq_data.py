4d9008e23009a72b0240ebe88ddf89fa8ffe4e67
elletim <113080146+elletim@users.noreply.github.com>
2022-10-11 00:03:06 -0700

    start connection

---
+++ ingest_aq_data.py
@@ -0,0 +1,8 @@
+from msilib.schema import Binary
+import psycopg2
+
+connection = psycopg2.connect("dbname=aq user=postgres")
+cursor = connection.cursor()
+
+cursor.execute("SELECT * FROM aq_data;")
+records = cursor.fetchall()
