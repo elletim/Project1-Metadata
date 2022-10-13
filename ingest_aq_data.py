9dc276c5b0e0ea736430d58ed1d89ec07ab80f4c
elletim <113080146+elletim@users.noreply.github.com>
2022-10-13 16:12:39 -0700

    change types of values

--- ingest_aq_data.py
+++ ingest_aq_data.py
@@ -8,7 +8,6 @@ urls = ["http://berkeleyearth.lbl.gov/air-quality/maps/cities/United_States/Cali
         "http://berkeleyearth.lbl.gov/air-quality/maps/cities/United_States/California/San_Diego.txt",
        "http://berkeleyearth.lbl.gov/air-quality/maps/cities/United_States/New_York/New_York_City.txt",
        "http://berkeleyearth.lbl.gov/air-quality/maps/cities/India/NCT/New_Delhi.txt"]
-#url = "http://berkeleyearth.lbl.gov/air-quality/maps/cities/United_States/California/Los_Angeles.txt"
 
 for url in urls:
     page = req.get(url) 
@@ -21,17 +20,19 @@ for url in urls:
     country =get_value(0)
     city = get_value(1)
     region = get_value(3)
-    lat = get_value(6)
-    long = get_value(7)
+    lat = int(float(get_value(6)))
+    long = int(float(get_value(7)))
     timezone= get_value(8)
-    city_id=  region+city
+    #city_id = str(1) + city 
+    city_id= " ".join([city , region])
     print(city_id)
     
 connection = psycopg2.connect("dbname=aq user=postgres")
 cursor = connection.cursor()
-#cursor.execute(" " "
+#cursor.execute("
  #INSERT INTO aq_meta (city, country, region, lat, long, timezone, city_id) 
- #VALUES (%s, %s, %s, %s, %s, %s, %s); ())
+ #VALUES (%s, %s, %s, %s, %s, %s, %s)" 
+ # (city, country, region, lat, long, timezone, city_id))
 cursor.execute("SELECT * FROM aq_data;")
 records = cursor.fetchall()
 #print(records)
