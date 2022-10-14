06feff058ca08332f428ef5a40e774c59d511b26
elletim <113080146+elletim@users.noreply.github.com>
2022-10-14 16:15:16 -0700

    finalize aq_meta,start reading values for aq_data

--- ingest_aq_data.py
+++ ingest_aq_data.py
@@ -3,36 +3,46 @@ import pandas as pd
 import requests as req
 from io import StringIO
 
-#/Users/elletimmer/Downloads/Project 1 Metadata/envmeta/bin/activate"
+
+def get_values(url):
+        page = req.get(url) 
+        page_df= pd.read_csv(StringIO(page.text),skiprows=10,sep='\t')
+        page_df.columns= ['Year','Month','Day', 'UTC Hour','PM2.5','PM10_mask','Retrospective']     
+        page_df = page_df[["Year","Month","Day","PM2.5",]]
+        df = pd.DataFrame(data=page_df)
+        df["Date"] = pd.to_datetime(df[["Year","Month","Day"]])
+        pm2_5 = df["PM2.5"].to_list()
+        datetime = df["Date"]
+        return(datetime)
+def get_city(url):
+        page = req.get(url) 
+        page_data = page.text
+        page_line = page_data.split('\n')
+        line= page_line[1]
+        city = line.split(":",1)[1]
+        return(city)
+
+
 urls = ["http://berkeleyearth.lbl.gov/air-quality/maps/cities/United_States/California/Los_Angeles.txt", 
         "http://berkeleyearth.lbl.gov/air-quality/maps/cities/United_States/California/San_Diego.txt",
-       "http://berkeleyearth.lbl.gov/air-quality/maps/cities/United_States/New_York/New_York_City.txt",
+       "http://berkeleyearth.lbl.gov/air-quality/maps/cities/United_States/New_York/New_York.txt",
        "http://berkeleyearth.lbl.gov/air-quality/maps/cities/India/NCT/New_Delhi.txt"]
-
 for url in urls:
-    page = req.get(url) 
-    page_data = page.text
-    page_line = page_data.split('\n')
-    def get_value(line):
-        line= (page_line[line])
-        value = line.split(":",1)[1]
-        return value
-    country =get_value(0)
-    city = get_value(1)
-    region = get_value(3)
-    lat = int(float(get_value(6)))
-    long = int(float(get_value(7)))
-    timezone= get_value(8)
-    #city_id = str(1) + city 
-    city_id= " ".join([city , region])
-    print(city_id)
-    
-connection = psycopg2.connect("dbname=aq user=postgres")
-cursor = connection.cursor()
-#cursor.execute("
- #INSERT INTO aq_meta (city, country, region, lat, long, timezone, city_id) 
- #VALUES (%s, %s, %s, %s, %s, %s, %s)" 
- # (city, country, region, lat, long, timezone, city_id))
-cursor.execute("SELECT * FROM aq_data;")
-records = cursor.fetchall()
+        #print(get_city(url))
+        print(get_values(url))
+#connection = psycopg2.connect("dbname=aq user=postgres")
+#cursor = connection.cursor()
+#cursor.execute("DROP table IF EXISTS aq_data")
+#cursor.execute("CREATE TABLE aq_data (city_id int REFERENCES aq_meta (city_id), datetime timestamp, pm2_5 float);")
+#select city_id from aq_meta where city = city 
+#cursor.execute('''INSERT INTO aq_meta (city_id, country, datetime, pm2_5) VALUES (%s, %s, %s)''', (city_id, dateime, pm2_5))
+#cursor.execute("SELECT * FROM aq_meta;")
+#records = cursor.fetchall()
 #print(records)
+#connection.commit()
+    
+#cursor.close()
+#connection.close()
+
+
+
--- ingest_aq_meta.py
+++ ingest_aq_meta.py
@@ -2,18 +2,49 @@ import psycopg2
 import pandas as pd 
 import requests as req
 from io import StringIO
+#/Users/elletimmer/Downloads/Project 1 Metadata/envmeta/bin/activate"
 
-urls = ["http://berkeleyearth.lbl.gov/air-quality/maps/cities/United_States/California/Los_Angeles.txt", 
+
+def get_value(page_line, line_num):
+        line= (page_line[line_num])
+        value = line.split(":",1)[1]
+        return value
+
+def get_meta(urls):
+    meta = []
+    for indx, url in enumerate(urls):
+        page = req.get(url) 
+        page_data = page.text
+        page_line = page_data.split('\n')
+        country =get_value(page_line,0).strip().replace("\r", "")
+        city = get_value(page_line, 1).strip().replace("\r", "")
+        region = get_value(page_line,3).strip().replace("\r", "")
+        lat = float(get_value(page_line,6))
+        long = float(get_value(page_line,7))
+        timezone= get_value(page_line,8).strip().replace("\r", "")
+        city_id = indx
+        meta.append([city, country, region, lat, long, timezone, city_id])
+    return(meta)
+
+if __name__ == "__main__":
+    urls = ["http://berkeleyearth.lbl.gov/air-quality/maps/cities/United_States/California/Los_Angeles.txt", 
         "http://berkeleyearth.lbl.gov/air-quality/maps/cities/United_States/California/San_Diego.txt",
-       "http://berkeleyearth.lbl.gov/air-quality/maps/cities/United_States/New_York/New_York.txt",
+       "http://berkeleyearth.lbl.gov/air-quality/maps/cities/United_States/New_York/New_York_City.txt",
        "http://berkeleyearth.lbl.gov/air-quality/maps/cities/India/NCT/New_Delhi.txt"]
 
-url = "http://berkeleyearth.lbl.gov/air-quality/maps/cities/United_States/California/Los_Angeles.txt"
-#for url in urls: 
-page = req.get(url) 
-page_df= pd.read_csv(StringIO(page.text),skiprows=10,sep='\t')
-page_df.columns= ['Year','Month','Day', 'UTC Hour','PM2.5','PM10_mask','Retrospective']     
-page_df = page_df[["Year","Month","Day","PM2.5","PM10_mask"]]
-df = pd.DataFrame(data=page_df)
-df["Date"] = pd.to_datetime(df[["Year","Month","Day"]])
-print(df.head())
+    meta = get_meta(urls)
+    
+    connection = psycopg2.connect("dbname=aq user=postgres")
+    cursor = connection.cursor()
+    cursor.execute("DROP table IF EXISTS aq_meta")
+    cursor.execute("CREATE TABLE aq_meta (city varchar(50), country varchar(50), region varchar(50), lat float, long float, time_zone varchar(50), city_id int, PRIMARY KEY(city_id));")
+    for city,country,region, lat, long, timezone, city_id in meta:
+
+        cursor.execute('''INSERT INTO aq_meta (city, country, region, lat, long, time_zone, city_id) VALUES (%s, %s, %s, %s, %s, %s, %s)''', (city, country, region, lat, long, timezone, city_id))
+        cursor.execute("SELECT * FROM aq_meta;")
+        records = cursor.fetchall()
+        print(records)
+        connection.commit()
+    
+    cursor.close()
+    connection.close()
